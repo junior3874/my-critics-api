d462d5da99c0b4fe22bf971a904ef8a1b884a255
Wellington Rufino <wellingtonjr53@outlook.com.br>
2022-06-13 14:58:11 -0300

    create spotify adapter

--- src/infra/services/spotify/spotify-adapter.ts
+++ src/infra/services/spotify/spotify-adapter.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import axios from "axios";
 
 import Album from "@/domain/entity/value-objects/Album";
@@ -10,48 +11,96 @@ import { IServiceRequestAudioFeatures } from "@/useCases/interfaces/services/ISe
 
 const api = axios.create({
   headers: {
-    Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`,
+    Authorization: `Bearer 44545515`,
   },
 });
 
+export const generateAccessToken = async () => {
+  const token = Buffer.from(
+    process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
+  ).toString("base64");
+
+  const params = new URLSearchParams();
+  params.append("grant_type", "client_credentials");
+  const res = await axios.post(
+    "https://accounts.spotify.com/api/token",
+    "grant_type=client_credentials",
+    {
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${token}`,
+      },
+    }
+  );
+  console.log(res.data.access_token);
+  return res.data.access_token;
+};
+
+const validRequest = async (command: Function) => {
+  api.interceptors.request.use(async function (config) {
+    config.headers.Authorization = `Bearer ${await generateAccessToken()}`;
+    return config;
+  });
+
+  return await command();
+};
+
 function SpotifyAdapter() {
   const requestMusic: IServiceRequestMusic = {
-    async load(musicId: string): Promise<Music> {
-      const res: any = await api.get(
-        `https://api.spotify.com/v1/tracks/${musicId}`
-      );
-      return {
-        author: res.artists.name,
-        duration: res.duration_ms,
-        name: res.name,
-        linkInPlatform: res.uri,
-        previewUrl: res.preview_url,
-      };
+    async load(musicId: string): Promise<IServiceRequestMusic.Result> {
+      try {
+        const res: any = await api.get(
+          `https://api.spotify.com/v1/tracks/${musicId}`
+        );
+        return {
+          duration: res.data.duration_ms,
+          image: res.data.album.images[0].url,
+          linkInPlatform: res.data.external_urls.spotify,
+          previewUrl: res.data.preview_url,
+          author: res.data.artists[0].name,
+          name: res.data.name,
+        };
+      } catch (err) {
+        if (err.response.status == "401") {
+          return validRequest(() => requestMusic.load(musicId));
+        }
+        return;
+      }
     },
   };
 
   const requestAlbum: IServiceRequestAlbum = {
-    async load(albumId: string): Promise<Album> {
-      const res: any = await api.get(
-        `https://api.spotify.com/v1/albums/${albumId}`
-      );
+    async load(albumId: string): Promise<IServiceRequestAlbum.Resut> {
+      try {
+        const res: any = await api.get(
+          `https://api.spotify.com/v1/albums/${albumId}`
+        );
 
-      const albumTracks = res.tracks.map(({}) => ({}));
-      return { image: res.images[0], name: res.name, tracks: [] };
+        return res.data;
+      } catch (err) {
+        if (err.response.status == "401") {
+          return validRequest(() => requestAlbum.load(albumId));
+        }
+        return;
+      }
     },
   };
 
   const searchTrackOrAlbum: IServiceSearchTrackOrAlbum = {
-    async search(query: string): Promise<IServiceSearchTrackOrAlbum.Result> {
+    async search(
+      data: IServiceSearchTrackOrAlbum.Params
+    ): Promise<IServiceSearchTrackOrAlbum.Result> {
       const res: unknown = await api
-        .get(`https://api.spotify.com/v1/search?type=album,track&q=${query}`)
+        .get(
+          `https://api.spotify.com/v1/search?type=album,track&q=${data.query}`
+        )
         .then((res) => res);
       return;
     },
   };
 
   const getAudioFeatures: IServiceRequestAudioFeatures = {
-    async load(): Promise<Music> {
+    async load(index: number): Promise<Music[]> {
       return;
     },
   };
